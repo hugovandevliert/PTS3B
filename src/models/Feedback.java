9a54901fbd01b83a0bc82cc486547abd521ae305
Mootjee1997 <26461265+Mootjee1997@users.noreply.github.com>
2017-09-27 11:55:18 +0200

    Javadoc

--- src/models/Feedback.java
+++ src/models/Feedback.java
@@ -3,12 +3,30 @@ package models;
 import java.time.LocalDateTime;
 
 public class Feedback {
-
+    /**
+     * LocalDateTime value that holds the datum value of when the feedback is given
+     */
     private LocalDateTime date;
+    /**
+     * Profile value that holds the Profile-object of the author that gave the feedback
+     */
     private Profile author;
+    /**
+     * String value that holds the message that the author gave as feedback
+     */
     private String message;
+    /**
+     * Boolean value that indicates whether the feedback given was positive or negative, (Like/Dislike).
+     */
     private boolean isPositive;
 
+    /**
+     * Constructor for making a new Feedback-object, it requires the following parameter input: Author, Date, isPositive, Message.
+     * @param author: The author of the Feedback
+     * @param date: The date that the Feedback was made
+     * @param isPositive: Whether the given Feedback was true
+     * @param message: The String message that belongs to the Feedback
+     */
     Feedback(Profile author, LocalDateTime date, boolean isPositive, String message) {
         this.author = author;
         this.date = date;
@@ -16,14 +34,30 @@ public class Feedback {
         this.message = message;
     }
 
+    /**
+     * Method that gets the Date of when the Feedback was given
+     * @return: Returns the Date of when the Feedback was made
+     */
     public LocalDateTime getDate() {
         return date;
     }
 
+    /**
+     * Method that gets the Profile-object that belongs to the Author that made the Feedback
+     * @return: Returns the Profile-object that belongs to the Author
+     */
     public Profile getAuthor() { return author; }
 
+    /**
+     * Method that gets the Message that belongs to this Feedback-object
+     * @return: Returns the Message that belongs to this Feedback
+     */
     public String getMessage() { return message; }
 
+    /**
+     * Method that gets the attribute belonging to this Feedback-object, whether the author is positive or not. It returns a boolean output
+     * @return: Returns the boolean value of whether the Feedback is positive or not
+     */
     public boolean isPositive() {
         return isPositive;
     }
--- src/models/User.java
+++ src/models/User.java
@@ -3,45 +3,95 @@ package models;
 import javafx.scene.image.Image;
 
 public class User {
-
+    /**
+     * Variabeles for saving the Username, Name and Email of a User-Object
+     */
     private String username, name, email;
+    /**
+     * Profile object that belongs to an User-object
+     */
     private Profile profile;
 
+    /**
+     * Constructor of the User-object which is used for creating an instance, it requires the following input parameters: Username, Name en Email
+     * @param username: The Username of an User-object, Username is an unique String value
+     * @param name: The Name of the User
+     * @param email: The Email of the User, this value should be unique
+     */
     User(String username, String name, String email) {
         this.username = username;
         this.name = name;
         this.email = email;
     }
 
+<<<<<<< Updated upstream
     public boolean changePassword(String newPassword) {
         return false;
     }
+=======
+    /**
+     * Method for changing the password of an User, it requires a String value that holds the new password and will return a boolean value back
+     * @param password: The new password that the User's password should be changed to. Password must have atleast: 6 characters and max. 32, 1 lowercase, 1 uppercase, 1 symbol, 1 integer
+     * @return: Depending on whether the new password is allowed and the outcome of the check, the method will return true when password is succesfully changed
+     */
+    public boolean changePassword(String password) { return false; }
+>>>>>>> Stashed changes
 
+    /**
+     * Method for getting the Username of this User-object
+     * @return: Will return the Username value of this User-object
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Method for getting the Name of this User-object
+     * @return: Will return the Name value of this User-object
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Method for changing the Name of this User-object, this requires a String parameter Name that consist of the new Name it should be changed to
+     * @param name: String value of the new Name this User-object should hold
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Method for getting the Email of this User-object
+     * @return: Will return the Email value of this User-object
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Method for changing the Email of this User-object, it requires a String input of the new Email value it should hold
+     * @param email: The new Email value that this User-object should hold. Requirements: Max. 255 characters, '@', '.' and a legitimate domain (ex.: .NL)
+     * @return: Depending on whether the new Email is allowed and accepted it will return a boolean value. The method will return true when it is succesfully changed
+     */
     public boolean setEmail(String email) {
         this.email = email;
         return false;
     }
 
+    /**
+     * Method for changing the Photo of this User object, it requires a Image input of the new Photo it should hold
+     * @param photo: The new Image value that this User-object should hold as photo. Value may not be null or return value will be false
+     * @return: Depending on whether the Image is accepted it will reutrn a boolean value. The method will return true when it s succesfully changed
+     */
     public boolean setPhoto(Image photo) {
         return false;
     }
 
+    /**
+     * Method for getting the Profile-object that belongs to this User-object
+     * @return: Returns the Profile-object of this User-object
+     */
     public Profile getProfile() {
         return null;
     }
