159c7d0ab9333139dab7515080d4ee78407f98e6
Thomas <thomasjansen10@hotmail.com>
2017-09-27 09:47:34 +0200

    + Added test-first unittests.

---
+++ UnitTests/models/FeedbackTest.java
@@ -0,0 +1,24 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FeedbackTest {
+    @Test
+    void getDate() {
+    }
+
+    @Test
+    void getAuthor() {
+    }
+
+    @Test
+    void getMessage() {
+    }
+
+    @Test
+    void isPositive() {
+    }
+
+}
