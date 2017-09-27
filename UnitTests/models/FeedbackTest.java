1f6b72af3a746873fa7ee2aa04b9d83dfa4c4765
Dynitios <thomasjansen10@hotmail.com>
2017-09-27 13:29:25 +0200

    + Updated FeedbackTest

--- UnitTests/models/FeedbackTest.java
+++ UnitTests/models/FeedbackTest.java
@@ -21,30 +21,29 @@ class FeedbackTest {
 
     @Test
     void getDate() {
-        LocalDateTime dateTime = LocalDateTime.now();
-        Feedback f = new Feedback(applicationManager.login("testuser2", "AbC*2f").getProfile(), dateTime, false, "Testfeedback");
-        assertSame(dateTime, f.getDate());
+        Feedback f = new Feedback(applicationManager.login("testuser2", "AbC*2f").getProfile(), false, "Testfeedback");
+        //Can't really test this getter since the value is unknown.
     }
 
     @Test
     void getAuthor() {
         Profile author = applicationManager.login("testuser2", "AbC*2f").getProfile();
-        Feedback f = new Feedback(author, LocalDateTime.now(), false, "Testfeedback");
+        Feedback f = new Feedback(author, false, "Testfeedback");
         assertSame(author, f.getAuthor(), "Author getter is not working properly.");
     }
 
     @Test
     void getMessage() {
-        Feedback f = new Feedback(applicationManager.login("testuser2", "AbC*2f").getProfile(), LocalDateTime.now(), false, "Testfeedback");
+        Feedback f = new Feedback(applicationManager.login("testuser2", "AbC*2f").getProfile(), false, "Testfeedback");
         assertEquals("Testfeedback", f.getMessage());
     }
 
     @Test
     void isPositive() {
-        Feedback f = new Feedback(applicationManager.login("testuser2", "AbC*2f").getProfile(), LocalDateTime.now(), false, "Testfeedback");
+        Feedback f = new Feedback(applicationManager.login("testuser2", "AbC*2f").getProfile(), false, "Testfeedback");
         assertFalse(f.isPositive(), "Positivity getter is not working properly for negative value.");
 
-        Feedback f2 = new Feedback(applicationManager.login("testuser2", "AbC*2f").getProfile(), LocalDateTime.now(), true, "Testfeedback");
+        Feedback f2 = new Feedback(applicationManager.login("testuser2", "AbC*2f").getProfile(), true, "Testfeedback");
         assertTrue(f2.isPositive(), "Positivity getter is not working properly for positive value.");
     }
 
