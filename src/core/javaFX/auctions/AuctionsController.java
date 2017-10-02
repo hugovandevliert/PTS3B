773c4d5f3ebcd8bfbcffcc4b438b873a9a4db1de
TimoF97 <t.fudala@student.fontys.nl>
2017-10-02 14:23:02 +0200

    Login UI Made

--- src/core/javaFX/auctions/AuctionsController.java
+++ src/core/javaFX/auctions/AuctionsController.java
@@ -1,6 +1,15 @@
 package core.javaFX.auctions;
 
 import core.javaFX.MenuController;
+import javafx.fxml.FXML;
 
 public class AuctionsController extends MenuController {
+
+    @FXML
+    void initialize() {
+        setIcons();
+
+        selectedMenu = imgviewAuctions;
+        imgviewAuctions.setImage(auctionsIconHovered);
+    }
 }
