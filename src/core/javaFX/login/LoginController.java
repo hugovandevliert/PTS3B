b6b874057d9396bdcf8409c2a59ab22168a134a2
TimoF97 <t.fudala@student.fontys.nl>
2017-10-03 17:11:12 +0200

    LoginController login methode - werkt niet

--- src/core/javaFX/login/LoginController.java
+++ src/core/javaFX/login/LoginController.java
@@ -2,6 +2,11 @@ package core.javaFX.login;
 
 import core.javaFX.MenuController;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+import java.io.File;
+import java.io.IOException;
 
 public class LoginController extends MenuController {
 
@@ -12,5 +17,12 @@ public class LoginController extends MenuController {
         //After login display auctions menu.
         selectedMenu = imgviewAuctions;
         imgviewAuctions.setImage(auctionsIconHovered);
+    }
+
+    public void login() throws IOException {
+        File resourcesDirectory = new File("src/core/javaFX/login/login.fxml");
+
+
+        Pane pane = FXMLLoader.load(getClass().getResource(String.valueOf(resourcesDirectory.toURI().toURL())));
     }
 }
