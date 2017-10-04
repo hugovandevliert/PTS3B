36dd3b4e26ab437725d8127bed65e35b7875f00f
Hugo <hugovdv@outlook.com>
2017-10-04 10:43:49 +0200

    Added check for empty textfields.

--- src/core/javaFX/login/LoginController.java
+++ src/core/javaFX/login/LoginController.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LoginController extends MenuController {
@@ -23,7 +24,13 @@ public class LoginController extends MenuController {
     }
 
     public void login() throws IOException {
+        if (txtUsername.getText().isEmpty() || txtPassword.getText().isEmpty()) {
+            System.out.println("VOER FF ALLES IN JONGE");
+            return;
+        }
+
         applicationManager.login(txtUsername.getText(), txtPassword.getText());
+
         if (applicationManager.isLoggedIn()) {
             paneContent.getChildren().clear();
             Pane newLoadedPane = FXMLLoader.load(getClass().getResource("/core/javafx/profile/profile.fxml"));
