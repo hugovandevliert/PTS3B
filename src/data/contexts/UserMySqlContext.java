6f6be91e4f59d119bc981a7619c511233326ba59
TimoF97 <t.fudala@student.fontys.nl>
2017-10-04 10:52:07 +0200

    Database Class - getData() Dynamically Implemented

--- src/data/contexts/UserMySqlContext.java
+++ src/data/contexts/UserMySqlContext.java
@@ -4,11 +4,14 @@ import data.interfaces.IUserContext;
 import javafx.scene.image.Image;
 import models.Profile;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import utilities.database.Database;
+
+import java.sql.PreparedStatement;
 
 public class UserMySqlContext implements IUserContext {
 
     @Override
-    public boolean setPhoto(Profile profile, Image photo) {
+    public boolean setPhoto (Profile profile, Image photo) {
         throw new NotImplementedException();
     }
 }
