1b880ebc4ccfaf37e5ff5a247bb62e074ccd0571
TimoF97 <t.fudala@student.fontys.nl>
2017-10-04 17:20:55 +0200

    ApplicationManager Code Quality Improved

--- src/core/ApplicationManager.java
+++ src/core/ApplicationManager.java
@@ -26,26 +26,25 @@ public class ApplicationManager {
     public User login(String username, String password) {
         String[] saltAndHash = userRepository.getSaltAndHash(username);
 
-        if(saltAndHash == null){
-            //Username not found or SQLException was caught
-            return null;
-        }
-        else{
-            if(hashString(password, saltAndHash[0]).equals(saltAndHash[1])){
+        if (saltAndHash != null){
+            if (hashString(password, saltAndHash[0]).equals(saltAndHash[1])){
                 return currentUser = userRepository.getUserByUsername(username);
             }
-            //Password incorrect
-            return null;
         }
+        //Password incorrect
+        return null;
     }
 
     public boolean registerUser(String username, String password, String email, String name) {
         Character[] characters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'V', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+
         SecureRandom secureRandom = new SecureRandom();
         StringBuilder saltStringBuilder = new StringBuilder();
-        for(int i = 0; i < 10; i++){
+
+        for(int i = 0; i < 16; i++){
             saltStringBuilder.append(characters[secureRandom.nextInt(characters.length)]);
         }
+
         String salt = saltStringBuilder.toString();
         return false; //userRepository.registerUser(username, hashString(password, salt), email, name, salt);
     }
@@ -56,6 +55,7 @@ public class ApplicationManager {
                 MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
                 String passwordToHash = "592" + password + salt;
                 messageDigest.update(passwordToHash.getBytes("UTF-8"));
+
                 byte[] digest = messageDigest.digest();
                 return String.format("%064x", new java.math.BigInteger(1, digest));
             }
@@ -64,7 +64,7 @@ public class ApplicationManager {
             }
             return null;
         }
-        throw new IllegalArgumentException("Password must be at least 6 characters, and salt should be exactly 10 characters");
+        throw new IllegalArgumentException("Password must be at least 6 characters, and SALT should be exactly 16 characters");
     }
 
     public boolean logout() {
