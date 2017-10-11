4250247dcbdd01591ecdd186f7d5e710caf129a9
Shari Joosten <shari.joosten@student.fontys.nl>
2017-10-11 12:32:53 +0200

    DAL Auction: Multiple improvements
    Still has 1 todo in it, which needs to be fixed in the near future, or
    we decide that Title is Unique.

--- src/data/interfaces/IAuctionContext.java
+++ src/data/interfaces/IAuctionContext.java
@@ -1,9 +1,6 @@
 package data.interfaces;
 
 import models.Auction;
-import models.Profile;
-import utilities.enums.Status;
-
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -13,6 +10,5 @@ public interface IAuctionContext {
     ArrayList<Auction> getAuctionsForSearchTerm(final String searchTerm) throws SQLException, IOException, ClassNotFoundException;
     Auction getAuctionForId(final int auctionId) throws SQLException, IOException, ClassNotFoundException;
     boolean addAuction(final Auction auction) throws SQLException;
-    boolean setStatus(final Status status, int auctionId);
-    boolean endAuction(final int auctionId);
+    boolean manuallyEndAuction(final int auctionId);
 }
