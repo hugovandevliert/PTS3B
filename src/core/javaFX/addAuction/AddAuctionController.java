50265c86c3ec6f3d66cda9a060eb6424674f3f93
Hugo <hugovdv@outlook.com>
2017-10-12 10:11:37 +0200

    Added validate method, added basic addauction layout

--- src/core/javaFX/addAuction/AddAuctionController.java
+++ src/core/javaFX/addAuction/AddAuctionController.java
@@ -1,16 +1,27 @@
 package core.javaFX.addAuction;
 
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 import core.javaFX.menu.MenuController;
+import javafx.fxml.FXML;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddAuctionController extends MenuController {
 
+    @FXML private JFXTextArea txtDescription;
+    @FXML private JFXTextField txtTitle;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
 
     public void createAuction() {
+    }
+
+    private boolean validate(String text)
+    {
+        return text.matches("[0-9]*");
     }
 }
