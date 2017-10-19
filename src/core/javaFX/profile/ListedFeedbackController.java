4b158ac262df5d42504b4a3c9920e5a3265ee763
TimoF97 <t.fudala@student.fontys.nl>
2017-10-19 14:51:20 +0200

    listedFeedback.fxml first draft

---
+++ src/core/javaFX/profile/ListedFeedbackController.java
@@ -0,0 +1,12 @@
+package core.javaFX.profile;
+
+import core.javaFX.menu.MenuController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ListedFeedbackController extends MenuController {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) { }
+}
