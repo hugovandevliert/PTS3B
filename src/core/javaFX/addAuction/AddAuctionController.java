80249462be058df15959e6023f01c9c3b524cf51
Hugo <hugovdv@outlook.com>
2017-10-24 23:14:16 +0200

    Partial completion of the AddAuction GUI

--- src/core/javaFX/addAuction/AddAuctionController.java
+++ src/core/javaFX/addAuction/AddAuctionController.java
@@ -1,6 +1,5 @@
 package core.javaFX.addAuction;
 
-import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import core.javaFX.menu.MenuController;
@@ -14,7 +13,9 @@ import utilities.enums.AlertType;
 import java.io.File;
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
@@ -23,8 +24,12 @@ public class AddAuctionController extends MenuController {
     @FXML private ImageView imgviewPicture1,imgviewPicture2, imgviewPicture3;
     @FXML private JFXTextField txtTitle;
     @FXML private JFXTextArea txtDescription;
-    @FXML private JFXDatePicker datepicker_Expiration, datepicker_Opening;
-
+    @FXML private JFXTextField txtStartTime;
+    @FXML private JFXTextField txtStartDate;
+    @FXML private JFXTextField txtStartPrice;
+    @FXML private JFXTextField txtEndTime;
+    @FXML private JFXTextField txtEndDate;
+    @FXML private JFXTextField txtIncrementation;
     private File[] images;
 
     @Override
@@ -57,19 +62,20 @@ public class AddAuctionController extends MenuController {
 
     public void createAuction() {
         try {
-            final boolean successful = applicationManager.currentUser.getProfile().addAuction(
-                    1,
-                    1,
-                    LocalDateTime.now().plusDays(1),
-                    LocalDateTime.now().plusSeconds(1),
-                    false,
-                    txtTitle.getText(),
-                    "Description",
-                    getImages(this.images)
-                    );
+            final String title = txtTitle.getText();
+            final String description = txtDescription.getText();
+            final LocalDateTime openingDate = LocalDateTime.of(convertToDate(txtStartDate.getText()), convertToTime(txtStartTime.getText()));
+            final LocalDateTime expirationDate = LocalDateTime.of(convertToDate(txtEndDate.getText()), convertToTime(txtEndTime.getText()));
+            final double startbid = convertToDouble(txtStartPrice.getText());
+            final double inrementation = convertToDouble(txtIncrementation.getText());
 
-            if (successful) MenuController.showAlertMessage("Successfully added auction!", AlertType.MESSAGE, 3000);
-            else MenuController.showAlertMessage("Your auction could not be added - Please try again!", AlertType.ERROR, 3000);
+            //TODO: add incrementation to addAuction method
+            //TODO: add minimum bid to GUI
+            final boolean successful = applicationManager.currentUser.getProfile().addAuction(startbid, 1, expirationDate,
+                    openingDate,false, title, description, getImages(this.images));
+
+            if (successful) MenuController.showAlertMessage("Auction added successfully!", AlertType.MESSAGE, 3000);
+            else MenuController.showAlertMessage("Your auction could not be added - Please try again.", AlertType.ERROR, 3000);
         } catch (IllegalArgumentException exception) {
             MenuController.showAlertMessage(exception.getMessage(), AlertType.ERROR, 3000);
         } catch (SQLException exception) {
@@ -95,7 +101,30 @@ public class AddAuctionController extends MenuController {
         return images;
     }
 
-    private boolean validate(final String text) {
-        return text.matches("[0-9]*");
+    private LocalTime convertToTime(final String time) {
+        if (time.matches( "([01]?[0-9]|2[0-3]):[0-5][0-9]")) {
+            return LocalTime.parse(time);
+        } else {
+            throw new IllegalArgumentException("Please enter a valid time");
+        }
+    }
+
+    private LocalDate convertToDate(final String date) {
+         if (date.matches("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|102]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))" +
+                    "(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|" +
+                    "[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])" +
+                    "|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$")) {
+             return LocalDate.parse(date);
+        } else {
+            throw new IllegalArgumentException("Please enter a valid date");
+        }
+    }
+
+    private double convertToDouble(final String text) {
+        if (text.matches("[0-9]*")) {
+            return Double.parseDouble(text);
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
